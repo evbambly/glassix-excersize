969507761b4da24cd25dfa824aef13c3b611097d
evbambly <45696895+evbambly@users.noreply.github.com>
2019-12-07 14:38:37 +0200

    Update environment.js
    Breaking the doc's logic, 
    so it's clearer what has to be fixed in order to make it work

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,8 @@
+//Insert relevant keys from the following APIs:
+//https://docs.ipdata.co
+//https://developers.google.com/maps/documentation/embed/guide
+
 export const environment = {
-    ipDataKey: 'Your key here',
-    googleApiKey: 'Your key here'
+    ipDataKey: Your key here,
+    googleApiKey: Your key here
 }
